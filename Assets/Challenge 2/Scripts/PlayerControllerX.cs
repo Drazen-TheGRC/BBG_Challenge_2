9178d7e0c807d659cdaed2c41b85a2e6e75e4749
Drazen Drinic <drazendrinic@hotmail.com>
2025-08-05 14:31:10 +0200

    Add: fire rate cooldown to dog spawn

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -6,12 +6,16 @@ public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
 
+    private float fireRate = 1.0f;   // seconds between fires
+    private float nextFire = 0.0f;   // next allowed fire time
+
     // Update is called once per frame
     void Update()
     {
-        // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        // Check for spacebar press AND if enough time has passed
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextFire)
         {
+            nextFire = Time.time + fireRate;  // reset the cooldown
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
         }
     }
